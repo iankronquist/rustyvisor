28052d8f8b2682fb98c817ddceaafcef17d17477
Ian Kronquist <iankronquist@gmail.com>
2017-01-22 18:53:02 -0500

    CLI guard object

---
+++ src/cli.rs
@@ -0,0 +1,16 @@
+use vmx;
+
+pub struct ClearLocalInterruptsGuard;
+
+impl ClearLocalInterruptsGuard {
+    pub fn new() -> Self {
+        vmx::cli();
+        ClearLocalInterruptsGuard{}
+    }
+}
+
+impl Drop for ClearLocalInterruptsGuard {
+    fn drop(&mut self) {
+        vmx::sti();
+    }
+}
