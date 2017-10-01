e815d331d221321ddfd3fa7a8b9a83dfc74c25a5
Ian Kronquist <iankronquist@gmail.com>
2017-09-30 20:03:29 -0700

    Try to whip dmesg logger into shape
    It's still broken, but slightly less so.

--- src/dmesg_logger.rs
+++ src/dmesg_logger.rs
@@ -1,11 +1,23 @@
 use linux;
 use log;
+use core::fmt::Write;
+use core::fmt;
 
 
 use log::{LogRecord, LogLevel, LogMetadata};
 
 #[derive(Default)]
-struct DMesgLogger(());
+struct DMesgLogger;
+
+impl fmt::Write for DMesgLogger {
+    fn write_str(&mut self, s: &str) -> Result<(), fmt::Error> {
+        unsafe {
+            linux::printk(cstring!("%s"), s.as_ptr());
+        }
+        Ok(())
+    }
+}
+
 
 impl log::Log for DMesgLogger {
     fn enabled(&self, metadata: &LogMetadata) -> bool {
@@ -14,23 +26,91 @@ impl log::Log for DMesgLogger {
 
     fn log(&self, record: &LogRecord) {
         if self.enabled(record.metadata()) {
-            let log_message = format!("{}: {}\0", record.level(), record.args());
-            unsafe {
-                linux::printk(cstring!("%s\n"), log_message.as_ptr());
-            }
+            let _ = write!(&mut DMesgLogger, "{}", record.args());
         }
     }
 }
 
+static LOGGER: DMesgLogger = DMesgLogger;
+
 pub fn init() -> Result<(), log::SetLoggerError> {
     unsafe {
         log::set_logger_raw(|max_log_level| {
-            static LOGGER: DMesgLogger = DMesgLogger(());
-            max_log_level.set(log::LogLevelFilter::Info);
+            max_log_level.set(log::LogLevelFilter::Debug);
             &LOGGER
         })
     }
 }
+
+pub fn fini() { }
+
 pub fn shutdown() -> Result<(), log::ShutdownLoggerError> {
     log::shutdown_logger_raw().map(|_logger| {})
+}
+
+// NULL terminate log messages so they can safely be fed to printk.
+#[macro_export]
+macro_rules! error {
+    (target: $target:expr, $fmt:tt, $($arg:tt)*) => (
+        log!(target: $target, log::LogLevel::Error, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt, $($arg:tt)*) => (
+        log!(log::LogLevel::Error, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt) => (
+        log!(log::LogLevel::Error, concat!($fmt, "\0"));
+    )
+}
+
+
+
+#[macro_export]
+macro_rules! warn {
+    (target: $target:expr, $fmt:tt, $($arg:tt)*) => (
+        log!(target: $target, log::LogLevel::Warn, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt, $($arg:tt)*) => (
+        log!(log::LogLevel::Warn, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt) => (
+        log!(log::LogLevel::Warn, concat!($fmt, "\0"));
+    )
+}
+
+#[macro_export]
+macro_rules! info {
+    (target: $target:expr, $fmt:tt, $($arg:tt)*) => (
+        log!(target: $target, log::LogLevel::Info, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt, $($arg:tt)*) => (
+        log!(log::LogLevel::Info, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt) => (
+        log!(log::LogLevel::Info, concat!($fmt, "\0"));
+    )
+}
+
+#[macro_export]
+macro_rules! debug {
+    (target: $target:expr, $fmt:tt, $($arg:tt)*) => (
+        log!(target: $target, log::LogLevel::Debug, concat!($fmt, "\0"), $($arg)*);
+    ); ($fmt:tt, $($arg:tt)*) => (
+        log!(log::LogLevel::Debug, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt) => (
+        log!(log::LogLevel::Debug, concat!($fmt, "\0"));
+    )
+}
+
+#[macro_export]
+macro_rules! trace {
+    (target: $target:expr, $fmt:tt, $($arg:tt)*) => (
+        log!(target: $target, log::LogLevel::Trace, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt, $($arg:tt)*) => (
+        log!(log::LogLevel::Trace, concat!($fmt, "\0"), $($arg)*);
+    );
+    ($fmt:tt) => (
+        log!(log::LogLevel::Trace, concat!($fmt, "\0"));
+    )
 }
