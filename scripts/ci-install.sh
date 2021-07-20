9519af4b1d0fddc11bd53669396178233427a44b
Ian Kronquist <iankronquist@gmail.com>
2021-07-19 21:49:39 -0700

    blah

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -3,24 +3,6 @@
 set -x
 set -e
 
-if [[ ! -e $HOME/.cargo/bin/rustup ]]; then
-  curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$TRAVIS_RUST_VERSION -y;
-fi
-
 source ~/.cargo/env
-
-rustup install nightly
-
-rustup default nightly
-
-rustup component add rust-src
-
-rustup component add clippy
-
-rustup component add rustfmt
-
-if ! hash cargo-xbuild > /dev/null 2>&1; then
-    cargo install cargo-xbuild
-fi
 
 sudo apt-get install linux-headers-$(uname -r)
