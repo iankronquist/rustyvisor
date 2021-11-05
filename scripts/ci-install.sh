7760db7979b748583ec7447608a6cb08d596fdfb
Ian Kronquist <iankronquist@gmail.com>
2021-11-04 17:35:20 -0700

    More rustup stuff

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -8,6 +8,10 @@ source ~/.cargo/env
 # Install Linux Kernel headers
 sudo apt-get install linux-headers-$(uname -r)
 
+# Install and default to a nightly version of Rust
+rustup install nightly
+rustup default nightly
+
 # Install UEFI Rust target
 rustup target install x86_64-unknown-uefi
 # Install Rust source code
