aefb618353a47a5d105fd5ba2a17b972e54848ca
Ian Kronquist <iankronquist@gmail.com>
2021-11-04 17:32:19 -0700

    Add rustup deps to CI install process

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -5,4 +5,10 @@ set -e
 
 source ~/.cargo/env
 
+# Install Linux Kernel headers
 sudo apt-get install linux-headers-$(uname -r)
+
+# Install UEFI Rust target
+rustup target install x86_64-unknown-uefi
+# Install Rust source code
+rustup component add rust-src
